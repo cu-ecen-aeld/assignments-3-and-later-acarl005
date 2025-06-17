1c08e1f67c39aa88d6bb97084aea1f8729887ddc
andy <2yinyang2@gmail.com>
2025-06-17 12:26:09 +0000

    complete assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -9,15 +9,7 @@
 */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+    return system(cmd) == 0;
 }
 
 /**
@@ -45,23 +37,32 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
+    bool success = false;
+    int pid = fork();
+    switch (pid) {
+        case -1:
+            perror("fork");
+            break;
+        case 0:
+            execv(command[0], &command[0]);
+            perror("execv");
+	    _exit(errno);
+        default:
+	    int status;
+            int result = waitpid(pid, &status, 0);
+            if (result == -1) {
+                perror("waitpid");
+            } else {
+		if (WIFEXITED(status)) {
+                    int code = WEXITSTATUS(status);
+                    success = code == 0;
+		}
+            }
+    }
     va_end(args);
 
-    return true;
+    return success;
 }
 
 /**
@@ -84,16 +85,42 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
+    bool success = false;
+    int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if (fd == -1) {
+        perror("open");
+    } else {
+        int pid = fork();
+        switch (pid) {
+            case -1:
+                perror("fork");
+                break;
+            case 0:
+                if (dup2(fd, 1) < 0) {
+                    perror("dup2");
+                    return false;
+                }
+                close(fd);
+                execv(command[0], &command[0]);
+                perror("execv");
+	        _exit(errno);
+            default:
+		int status;
+                int result = waitpid(pid, &status, 0);
+                if (result == -1) {
+                    perror("waitpid");
+                } else {
+		    if (WIFEXITED(status)) {
+                        int code = WEXITSTATUS(status);
+                        success = code == 0;
+		    }
+                }
+        }
+    }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
-
+    close(fd);
     va_end(args);
 
-    return true;
+    return success;
 }
+
